152f39b918136b30a9ece94a8f9349f8104c0db5
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-05-14 12:57:37 +0300

    Exercise 1.1

---
+++ part1/src/App.jsx
@@ -0,0 +1,47 @@
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+            <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+            <p>{props.parts[2].name} {props.parts[2].exercises}</p>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.total}</p>
+    )
+}
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    const parts = [
+        { name: part1, exercises: exercises1 },
+        { name: part2, exercises: exercises2 },
+        { name: part3, exercises: exercises3 }
+    ]
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={parts} />
+            <Total total={exercises1 + exercises2 + exercises3} />
+        </div>
+    )
+}
+
+export default App
