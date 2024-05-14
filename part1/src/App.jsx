9eee3828ca47f8b13e9faf38e6edb5cdddacc83b
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-05-14 14:48:06 +0300

    Exercise 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,55 +1,32 @@
-const Header = (props) => {
-    console.log(props)
+import { useState } from 'react'
 
-    return <h1>{props.name}</h1>
-}
+const Button = ({text, handleClick}) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
 
-const Part = (props) => {
-    return <p>{props.part.name} {props.part.exercises}</p>
-}
-
-const Content = (props) => {
-    return (
-        <div>
-            <Part part={props.parts[0]} />
-            <Part part={props.parts[1]} />
-            <Part part={props.parts[2]} />
-        </div>
-    )
-}
-
-const Total = (props) => {
-    const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
-
-    return (
-        <p>Number of exercises {total}</p>
-    )
-}
+const Statistic = ({name, count}) => (
+    <p>{name} {count}</p>
+)
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
     return (
         <div>
-            <Header name={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
+            <h1>give feedback</h1>
+            <Button text={"good"} handleClick={() => setGood(good + 1)} />
+            <Button text={"neutral"} handleClick={() => setNeutral(neutral + 1)} />
+            <Button text={"bad"} handleClick={() => setBad(bad + 1)} />
+
+            <h1>statistics</h1>
+            <Statistic name={"good"} count={good} />
+            <Statistic name={"neutral"} count={neutral} />
+            <Statistic name={"bad"} count={bad} />
         </div>
     )
 }
