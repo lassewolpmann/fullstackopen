1d17086b006fe17e3c29e6608f7e67495ec9dce9
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-05-15 13:10:50 +0300

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
 import Persons from "./components/Persons.jsx";
 import InputForm from "./components/InputForm.jsx";
 import Filter from "./components/Filter.jsx";
 
-const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+import axios from "axios";
 
+const App = () => {
+    const [persons, setPersons] = useState([])
+
+    const hook = () => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }
+
+    useEffect(hook, [])
 
     const [filter, setFilter] = useState('')
 
