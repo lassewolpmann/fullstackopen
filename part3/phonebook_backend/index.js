6c60ea0b500d0b4e2d35532c5b0aeebd81bdc109
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-05-21 10:02:40 +0300

    Exercise 3.4 - corrected small mistake

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -54,7 +54,7 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const person = persons.find((person) => person.id === id)
+    persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
 })
