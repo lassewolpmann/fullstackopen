b272f6d5120b2679a235976d47f3c8067ebf37ed
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-05-21 09:57:19 +0300

    Exercise 3.2

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -34,6 +34,13 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const total_people = persons.length
+    const date = new Date()
+
+    response.send(`<p>Phonebook has info for ${total_people} people</p><p>${date}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
