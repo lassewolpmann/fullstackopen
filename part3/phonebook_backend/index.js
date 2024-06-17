f8d17f453d808c18510e4e5c7e2a20f420b6e8be
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-06-17 13:22:15 +0300

    Added Exercise 3.5

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -59,6 +59,28 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+    let maxID = 999999
+    const id = Math.floor(Math.random() * maxID)
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const person = {
+        "id": id,
+        "name": body.name,
+        "number": body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
