f14dcb0ff60e4d6f6778a661989927048e349cf9
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-06-19 10:12:47 +0300

    Exercise 3.9

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -16,6 +17,7 @@ app.use(morgan((tokens, req, res) => {
         ].join(' ')
     }
 }))
+app.use(cors())
 
 let persons = [
     {
