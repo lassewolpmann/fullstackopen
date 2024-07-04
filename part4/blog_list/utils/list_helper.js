464fe771a93deda1d4508506c7fae9e48ebda9eb
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-07-04 08:59:02 +0300

    Exercise 4.5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -8,7 +8,23 @@ const totalLikes = (blogs) => {
     }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    let sortedBlogs = blogs.sort((a, b) => {
+        // Sort in ascending order
+        return a.likes > b.likes ? 1 : -1
+    })
+
+    let favouriteBlog = sortedBlogs[sortedBlogs.length - 1]
+
+    return {
+        title: favouriteBlog.title,
+        author: favouriteBlog.author,
+        likes: favouriteBlog.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
