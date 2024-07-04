327af0a29ee66c012e3b13a4b349ecbfe05da361
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-07-04 09:18:18 +0300

    Exercises 4.6 and 4.7

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -23,8 +23,59 @@ const favoriteBlog = (blogs) => {
     }
 }
 
+const mostBlogs = (blogs) => {
+    // Create a set of author names
+    let authors = [...new Set(blogs.map(blog => blog.author))]
+
+    let blogsPerAuthor = authors.map(author => {
+        let blogCount = blogs.reduce((acc, blog) => {
+            if (blog.author === author) {
+                return acc + 1
+            } else {
+                return acc
+            }
+        }, 0)
+
+        return {
+            author: author,
+            blogs: blogCount
+        }
+    }).sort((a, b) => {
+        // Sort blog count in ascending order
+        return a.blogs > b.blogs ? 1 : -1
+    })
+
+    return blogsPerAuthor[blogsPerAuthor.length - 1]
+}
+
+const mostLikes = (blogs) => {
+    let authors = [...new Set(blogs.map(blog => blog.author))]
+
+    let likesPerAuthor = authors.map(author => {
+        let likeCount = blogs.reduce((acc, blog) => {
+            if (blog.author === author) {
+                return acc + blog.likes
+            } else {
+                return acc
+            }
+        }, 0)
+
+        return {
+            author: author,
+            likes: likeCount
+        }
+    }).sort((a, b) => {
+        // Sort likes count in ascending order
+        return a.likes > b.likes ? 1 : -1
+    })
+
+    return likesPerAuthor[likesPerAuthor.length - 1]
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
