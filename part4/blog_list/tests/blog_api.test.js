d68e17e57d678ecaeddebfb60f63ea3c5f2c05ed
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-07-10 09:55:28 +0300

    Fix to Exercise 4.8

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -1,15 +1,19 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
-    await api
+test('6 notes are returned as json', async () => {
+    const res = await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+
+    const blogs = res.body
+    assert.strictEqual(blogs.length, 6)
 })
 
 after(async () => {
