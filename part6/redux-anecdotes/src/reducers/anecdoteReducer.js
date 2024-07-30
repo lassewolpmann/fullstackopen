feadd7eccdd099304196e5dff362c91c9399326c
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-07-30 07:59:39 +0300

    Exercise 6.14

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -19,16 +10,13 @@ const asObject = (anecdote) => {
     }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
 const anecdoteSlice = createSlice({
     name: 'anecdotes',
-    initialState,
+    initialState: [],
     reducers: {
-        createAnecdote(state, action) {
-            const anecdote = action.payload
-            state.push(asObject(anecdote))
-        },
+        createAnecdote: (state, action) => state.push(asObject(action.payload)),
+        appendAnecdote: (state, action) => state.push(action.payload),
+        setAnecdotes: (state, action) => action.payload,
         voteFor(state, action) {
             const id = action.payload
             const anecdoteToChange = state.find(a => a.id === id)
@@ -38,5 +26,10 @@ const anecdoteSlice = createSlice({
     }
 })
 
-export const { createAnecdote, voteFor } = anecdoteSlice.actions
+export const {
+    createAnecdote,
+    appendAnecdote,
+    setAnecdotes,
+    voteFor
+} = anecdoteSlice.actions
 export default anecdoteSlice.reducer
