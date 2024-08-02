f001adb83782e83904442fa63cc9c8936053bae9
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-08-02 13:01:55 +0300

    Exercise 8.8

---
+++ part8/library-frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+
+const App = () => {
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <Router>
+      <div>
+        <Link style={padding} to="/">authors</Link>
+        <Link style={padding} to="/books">books</Link>
+        <Link style={padding} to="/add-book">add book</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Authors /> } />
+        <Route path="/books" element={<Books /> } />
+        <Route path="/add-book" element={<NewBook /> } />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
