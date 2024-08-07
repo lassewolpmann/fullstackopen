1d76fb4bc982a8ae37be644e5436b33933779d12
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-08-07 14:43:44 +0300

    Exercise 9.5

--- part9/first-steps/bmiCalculator.ts
+++ part9/first-steps/bmiCalculator.ts
@@ -1,23 +1,4 @@
-interface BmiCalcValues {
-  height: number,
-  weight: number
-}
-
-const parseBmiArguments = (args: string[]): BmiCalcValues => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3])
-    }
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
-}
-
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / Math.pow(height / 100, 2)
 
   let range = ""
@@ -35,15 +16,3 @@ const calculateBmi = (height: number, weight: number): string => {
 
   return `${range} range`
 }
-
-try {
-  const { height, weight } = parseBmiArguments(process.argv)
-  console.log(calculateBmi(height, weight))
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
-}
-
--- part9/first-steps/index.ts
+++ part9/first-steps/index.ts
@@ -1,9 +1,27 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query
+
+  if (!height || !weight) {
+    res.status(400).json({ error: "height or weight parameter is missing" })
+  } else if (isNaN(Number(height)) || isNaN(Number(weight))) {
+    res.status(400).json({ error: "height or weight parameter is not a number "})
+  } else {
+    const bmi = calculateBmi(Number(height), Number(weight))
+    res.status(200).json({
+      height: height,
+      weight: weight,
+      bmi: bmi
+    })
+  }
+})
 
 const PORT = 3003;
 
