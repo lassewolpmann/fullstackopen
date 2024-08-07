c62d2e9f6aedd20a20aa6260f022afe764017fc3
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-08-07 11:26:51 +0300

    Exercise 9.2

---
+++ part9/first-steps/exerciseCalculator.ts
@@ -0,0 +1,44 @@
+interface Result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+const calculateExercises = (hours: number[], target: number): Result => {
+  const period = hours.length
+  const trainingDays = hours.filter((hour: number): boolean => hour !== 0).length
+  const average = hours.reduce((acc, cur) => acc + cur) / period
+
+  let rating = 0
+  let ratingDesc = ''
+
+  if (average < (target - target / 10)) {
+    rating = 1
+    ratingDesc = 'you did not reach your target'
+  } else if (average < target) {
+    rating = 2
+    ratingDesc = 'you almost reached your target!'
+  } else if (average < (target + target / 10)) {
+    rating = 3
+    ratingDesc = 'you reached your target!'
+  } else if (average > (target + target / 10)) {
+    rating = 2
+    ratingDesc = 'you overreached your target, take it a bit easier next time'
+  }
+
+  return {
+    periodLength: period,
+    trainingDays: trainingDays,
+    success: average >= target,
+    rating: rating,
+    ratingDescription: ratingDesc,
+    target: target,
+    average: average
+  }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
