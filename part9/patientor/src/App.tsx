74248777addee01b29f0d81bd18464dbd321cab1
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-08-10 12:25:40 +0300

    Small fix to Exercise 9.21

--- part9/patientor/src/App.tsx
+++ part9/patientor/src/App.tsx
@@ -36,7 +36,7 @@ const App = () => {
           <Divider hidden />
           <Routes>
             <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
-            <Route path="/:id" element={<PatientPage patients={patients} />} />
+            <Route path="/:id" element={<PatientPage />} />
           </Routes>
         </Container>
       </Router>
