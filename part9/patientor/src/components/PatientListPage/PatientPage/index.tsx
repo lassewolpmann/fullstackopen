fc2399f05b833926c90f258e8af7abf9c1434b48
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-08-13 10:34:33 +0300

    Exercise 9.23

--- part9/patientor/src/components/PatientListPage/PatientPage/index.tsx
+++ part9/patientor/src/components/PatientListPage/PatientPage/index.tsx
@@ -32,6 +32,17 @@ const PatientPage = () => {
       </h1>
       <p>ssn: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
+      <h2>entries</h2>
+      {patient.entries.map(entry => (
+        <div key={entry.id}>
+          <p>{entry.date} <i>{entry.description}</i></p>
+          <ul>
+            {entry.diagnosisCodes?.map(code => (
+              <li key={code}>{code}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -4,6 +4,51 @@ export interface Diagnosis {
   latin?: string;
 }
 
+interface BaseEntry {
+  id: string;
+  date: string;
+  type: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+  description: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  discharge: {
+    date: string;
+    criteria: string;
+  }
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  }
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
+
 export enum Gender {
   Male = "male",
   Female = "female",
@@ -11,12 +56,13 @@ export enum Gender {
 }
 
 export interface Patient {
-  id: string;
-  name: string;
-  occupation: string;
-  gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+  id: string,
+  name: string,
+  dateOfBirth: string,
+  ssn: string,
+  gender: Gender,
+  occupation: string,
+  entries: Entry[]
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
